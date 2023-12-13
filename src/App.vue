7b0288df37e7f121cbb6feaaec63f3d4b89584ea
semilore <semilore@omotade-michaels.com>
2023-12-13 11:42:58 +0100

    feat: styling

--- src/App.vue
+++ src/App.vue
@@ -41,30 +41,29 @@ const handleAddMonth = () => {
   <div class="container">
     <h1>Calculate a new date</h1>
     <div class="current">
-      <p><span>Current Date: </span>{{ date }}</p>
+      <p class="current-date"><span>Current Date: </span>{{ date }}</p>
       <p>Current day: {{ `${getDay()}th` }}</p>
       <p>Current day of week: {{ getDayOfWeek() }}</p>
       <p>Current Month: {{ getMonth() }}</p>
 
     </div>
-    <div class="addDay">
-      <label for="addDay">Add Day: </label>
-      <input v-model="daysToAdd" type="number" placeholder="Number of days to add">
-      <button @click="handleAddDay">Add</button>
-    </div>
-    <div class="addMonth">
-      <label for="addMonth">Add Month: </label>
-      <input v-model="monthsToAdd" type="number" placeholder="Number of Months to add">
-      <button @click="handleAddMonth">Add</button>
+    <div class="form">
+      <div class="add">
+        <label for="addDay">Add Day: </label>
+        <input v-model="daysToAdd" type="number" placeholder="Number of days to add">
+        <button @click="handleAddDay">Add</button>
+      </div>
+      <div class="add">
+        <label for="addMonth">Add Month: </label>
+        <input v-model="monthsToAdd" type="number" placeholder="Number of Months to add">
+        <button @click="handleAddMonth">Add</button>
+      </div>
     </div>
     <div v-if="datesAdded.length > 0" v-for="dates in datesAdded" class="datesAdded">
-      <div class="" v-if="dates.daysAdded">Days Added: {{ dates.daysAdded }}</div>
-      <div class="" v-else-if="dates.monthsAdded">Months Added: {{ dates.monthsAdded }}</div>
+      <div class="days" v-if="dates.daysAdded">Days Added: {{ dates.daysAdded }}</div>
+      <div class="days" v-else-if="dates.monthsAdded">Months Added: {{ dates.monthsAdded }}</div>
       <div class="">
-        <p>New Date: {{ dates.newDate }}</p>
-        <!-- <p>New day: {{ `${getDay(dates)}th` }}</p>
-        <p>New day of week: {{ getDayOfWeek(dates) }}</p>
-        <p>New Month: {{ getMonth(dates) }}</p> -->
+        <p class="current-date"><span>New Date:</span> {{ dates.newDate }}</p>
       </div>
     </div>
     <div v-else class="">
@@ -73,4 +72,44 @@ const handleAddMonth = () => {
   </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+.current-date {
+  border: 1px solid white;
+  padding: 6px;
+  border-radius: 4px;
+}
+
+.form {
+  border: 1px solid white;
+  padding: 10px;
+  border-radius: 8px;
+}
+
+.days {
+  text-align: left;
+  font-weight: 700;
+  font-size: larger;
+}
+
+.current-date span {
+  font-weight: 700;
+}
+
+button {
+  padding-top: 0px;
+  padding-bottom: 0px;
+}
+
+.add {
+  height: 2.5rem;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 4px;
+}
+
+.add label {
+  font-weight: 700;
+  font-size: larger;
+}
+</style>
